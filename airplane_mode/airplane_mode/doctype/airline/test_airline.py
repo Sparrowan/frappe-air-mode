97b7bbc682700cdc5e23c1e7ad4764af0b7da329
Alphius <alphiuswambua@gmail.com>
2026-06-08 11:40:30 +0300

    Added airline doctype

---
+++ airplane_mode/airplane_mode/doctype/airline/test_airline.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Alphius Wambua and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAirline(FrappeTestCase):
+	pass
