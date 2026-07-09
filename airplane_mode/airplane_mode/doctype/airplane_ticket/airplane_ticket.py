4dff84e727a10d35ad24dc4fe2d417681200c137
Alphius <alphiuswambua@gmail.com>
2026-07-09 07:23:35 +0300

    Refactor Airplane Ticket: link to Airplane Flight, add seat/pricing/add-ons fields and controller

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,35 @@
 # Copyright (c) 2026, Alphius Wambua and contributors
 # For license information, please see license.txt
 
-# import frappe
+import random
+
+import frappe
 from frappe.model.document import Document
 
 
 class AirplaneTicket(Document):
-	pass
+	def before_insert(self):
+		self.seat = f"{random.randint(1, 100)}{random.choice('ABCDE')}"
+
+	def validate(self):
+		self.calculate_total_amount()
+		self.remove_duplicate_addons()
+
+	def before_submit(self):
+		if self.status != "Boarded":
+			frappe.throw("Cannot submit ticket. Ticket status must be 'Boarded' before submission.")
+
+	def calculate_total_amount(self):
+		total = self.flight_price or 0
+		for addon in self.add_ons:
+			total += addon.amount or 0
+		self.total_amount = total
+
+	def remove_duplicate_addons(self):
+		seen = set()
+		unique = []
+		for addon in self.add_ons:
+			if addon.item not in seen:
+				seen.add(addon.item)
+				unique.append(addon)
+		self.add_ons = unique
