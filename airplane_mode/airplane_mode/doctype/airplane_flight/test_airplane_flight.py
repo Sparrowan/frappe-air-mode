0eb2c365e3483450faf36c63ca039f521bdf6acf
Alphius <alphiuswambua@gmail.com>
2026-07-09 07:21:53 +0300

    Add Airplane Flight DocType with web view and submit controller

---
+++ airplane_mode/airplane_mode/doctype/airplane_flight/test_airplane_flight.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2026, Alphius Wambua and contributors
+# See license.txt
+
+import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class UnitTestAirplaneFlight(UnitTestCase):
+	pass
+
+
+class IntegrationTestAirplaneFlight(IntegrationTestCase):
+	pass
